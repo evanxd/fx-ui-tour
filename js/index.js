0bf9f9528f30ba0856f323411cd39a758ff6c20d
Evan Tseng <evan@tseng.io>
2017-04-13 15:12:13 +0800

    Add customizeFirefox, defaultBrowser, and privateBrowsing buttons

--- js/index.js
+++ js/index.js
@@ -2,12 +2,24 @@
 
 (function() {
   var addons = document.querySelector("#add-ons");
+  var customizeFirefox = document.querySelector("#customize-firefox");
+  var defaultBrowser = document.querySelector("#default-browser");
   var firefoxSync = document.querySelector("#firefox-sync");
   var oneOffSearch = document.querySelector("#one-off-search");
+  var privateBrowsing = document.querySelector("#private-browsing");
 
   addons.addEventListener("click", function() {
     document.dispatchEvent(new CustomEvent("mozUITour",
       {bubbles: true, detail: {action: "showMenu", data:{name: "appMenu"}}}));
+  });
+
+  customizeFirefox.addEventListener("click", function() {
+    document.dispatchEvent(new CustomEvent("mozUITour",
+      {bubbles: true, detail: {action: "showMenu", data:{name: "appMenu"}}}));
+  });
+
+  defaultBrowser.addEventListener("click", function() {
+    // TODO: Add a new action to open default browser setting panel in the UITour library.
   });
 
   firefoxSync.addEventListener("click", function() {
@@ -18,5 +30,10 @@
   oneOffSearch.addEventListener("click", function() {
     document.dispatchEvent(new CustomEvent("mozUITour",
       {bubbles: true, detail: {action: "openSearchPanel", data:{}}}));
+  });
+
+  privateBrowsing.addEventListener("click", function() {
+    document.dispatchEvent(new CustomEvent("mozUITour",
+      {bubbles: true, detail: {action: "showMenu", data:{name: "appMenu"}}}));
   });
 }());
