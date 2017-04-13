684f962fa97636057716f45c011620eaa4bdc111
Evan Tseng <evan@tseng.io>
2017-04-13 15:01:20 +0800

    Add Firefox Sync button

--- js/index.js
+++ js/index.js
@@ -2,11 +2,17 @@
 
 (function() {
   var addons = document.querySelector("#add-ons");
+  var firefoxSync = document.querySelector("#firefox-sync");
   var oneOffSearch = document.querySelector("#one-off-search");
 
   addons.addEventListener("click", function() {
     document.dispatchEvent(new CustomEvent("mozUITour",
       {bubbles: true, detail: {action: "showMenu", data:{name: "appMenu"}}}));
+  });
+
+  firefoxSync.addEventListener("click", function() {
+    document.dispatchEvent(new CustomEvent("mozUITour",
+      {bubbles: true, detail: {action: "showFirefoxAccounts", data:{name: "appMenu"}}}));
   });
 
   oneOffSearch.addEventListener("click", function() {
